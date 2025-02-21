14063316f03357ebbb2e3f894d933f501ddd433d
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-02-21 05:41:53 +0000

    stringify and parse

--- Week1/javascript/12_additional.js
+++ Week1/javascript/12_additional.js
@@ -1,3 +1,15 @@
 let a = 3, b = 4;
 let ans = a??b; // means (a !== null && a !== undefined) ? a : b;
 console.log(ans)
+
+//fun.call
+function sayHi() {
+    alert(this.name);
+  }
+  
+  let user = { name: "John" };
+  let admin = { name: "Admin" };
+  
+  // use call to pass different objects as "this"
+  sayHi.call( user ); // John
+  sayHi.call( admin ); // Admin
