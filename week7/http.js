ecf386b645e8e92ccfa1dd79efb25fd134e5e9cb
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-04-01 17:49:51 +0530

    http module

--- week7/http.js
+++ week7/http.js
@@ -7,7 +7,7 @@ var url = require('url');
 
 
 // http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.writeHead(200, {'Content-Type': 'text/html'}); 
 //   var q = url.parse(req.url, true).query;
 //   var txt = q.name + " " + q.lastname;
 //   res.end(txt);
