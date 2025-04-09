485f382f8550bbafa33a213af7addf1074a1cc12
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-04-09 17:32:59 +0530

    node modules

---
+++ week8/module/fs.js
@@ -0,0 +1,48 @@
+// var fs = require("fs");
+
+// var createStream = fs.createWriteStream("JournalDEV.txt");
+// createStream.end();
+
+// var fs = require("fs");
+
+// var writeStream = fs.createWriteStream("JournalDEV.txt");
+// writeStream.write("Hi, JournalDEV Users. ");
+// writeStream.write("Thank You.");
+// writeStream.end();
+
+// var fs = require("fs");
+
+// function readData(err, data) {
+// 	  console.log(data);
+// }
+
+// fs.readFile('JournalDEV.txt', 'utf8', readData);
+
+// var fs = require("fs");
+
+// fs.readFile('JournalDEV.txt', 'utf8', function(err, data) {
+// 	  console.log(data);
+// });
+
+// var fs = require("fs");
+
+// try{
+// fs.readFile('JournalDEV.txt', function(err, data) {
+// 	  console.log(data);
+// });} catch{
+//     console.log('error');
+// }
+
+// var fs = require("fs");
+
+// try{
+//     fs.appendFile('JournalDEV.tx','abcd', function(err, data) {
+//           console.log('done');});
+// } catch{
+//     console.log('error');
+// }
+
+var fs = require("fs");
+fs.writeFile('JournalllDEV.txt','abcf',function(err, data) {
+  console.log('done');
+});
