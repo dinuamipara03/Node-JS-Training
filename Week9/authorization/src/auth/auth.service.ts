0eecd52dbd675cf83b92712c6cefd15d6ffad7f4
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-04-29 11:12:35 +0530

    added week9

---
+++ Week9/authorization/src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    return this.usersService.validateUser(username, pass);
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async register(username: string, password: string) {
+    const user = await this.usersService.create(username, password);
+    return user;
+  }
+}
